0ba989e33347b7013c4e7d3819c3d0d8c2b65a02
BibekJoshi4000 <bibekjoshi4000@gmail.com>
2023-07-21 11:36:45 +0530

    operators in R

---
+++ part-1 (basic operations).R
@@ -0,0 +1,36 @@
+x <- 10   # assignig an value to a variable
+y <- 20    # assignig an value to a variable
+
+# using arithmatic operators
+x + y    #sum
+x - y    #differance
+x * y    #product
+x / y    #division
+x ^ y    #raised to the power
+x %% y   #modulus
+x %/% y  #integer division
+
+# using comparision operators
+x < y   #greater than
+x > y   #smaller than
+x >= y  #greater than or equal to
+x <= y  #smaller than or equal to
+x == y  #equal to
+x != y  #not equal to
+
+# using logical operator
+
+x && y  # and operator
+x || y  # or operator
+!x      # not operator
+
+# using bitwise operator
+
+bitwOr(x,y)     #bitwise OR
+bitwAnd(x,y)    #bitwise AND 
+bitwXor(x,y)    #bitwise Xor
+bitwNot(x)      #bitwise Not
+bitwShiftL(x,2) #bitwise shift left
+bitwShiftL(y,2) #bitwise shift left
+bitwShiftR(x,2) #bitwise shift right
+bitwShiftR(y,2) #bitwise shift right
